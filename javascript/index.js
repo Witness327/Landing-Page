ac095d51aaa40399a53d9a0c996986c16e5168a4
Andres Perez <andresperez327@gmail.com>
2020-10-20 01:31:58 -0400

    Tweaking the Nav bar so thatin mobile view it had a dropdown/hamburger menu. Also when above 768px the nave bar will not have the drop down but rather inline-block for the menu items in in those views removed the Landers.. text. When the hamburger is pressed and then minimized in tablet and desktop view the nav bar dissapears. Added an alert in the JavaScript to notify the user if they do not see the Nav bar in those views to refresh the page for the time being.

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,12 @@
 
 function myFunction() {
     var x = document.getElementById('my-links');
+    var y =window.matchMedia("(min-width: 768px)")
     if(x.style.display === "block") {
         x.style.display = "none";
     } else {
+        alert("Tip: If you go to tablet or desktop view and do not see the Nav Bar. Please refresh the page.");
         x.style.display = "block";
-    }
+    } 
+
 }
