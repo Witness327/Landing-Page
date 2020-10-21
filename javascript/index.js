fa9b9911660bfba678c0d8579c00862f1e0648d0
Andres Perez <andresperez327@gmail.com>
2020-10-21 00:14:43 -0400

    Was able to keep nav bar from disapearing in tablet and desktop view after the hamburger was opened and then closed in mobile view.

--- javascript/index.js
+++ javascript/index.js
@@ -2,11 +2,11 @@
 function myFunction() {
     var x = document.getElementById('my-links');
     var y =window.matchMedia("(min-width: 768px)")
+
     if(x.style.display === "block") {
         x.style.display = "none";
     } else {
-        alert("Tip: If you go to tablet or desktop view and do not see the Nav Bar. Please refresh the page.");
-        x.style.display = "block";
+         x.style.display = "block";
     } 
 
 }
